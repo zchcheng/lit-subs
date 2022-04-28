346e6817b464a3d785f8883366394bb2c2184520
Chia-Hao (Zack) Cheng <88179158+zchcheng@users.noreply.github.com>
2022-04-28 12:27:32 -0700

    Time: 120 ms (15.77%), Space: 50.7 MB (83.21%) - LeetHub

--- 973-k-closest-points-to-origin/973-k-closest-points-to-origin.java
+++ 973-k-closest-points-to-origin/973-k-closest-points-to-origin.java
@@ -1,23 +1,56 @@
 class Solution {
+    int[][] points;
+    
     public int[][] kClosest(int[][] points, int k) {
-        PriorityQueue<int[]> pq = new PriorityQueue<>((int[] a, int[] b) -> {
-            return (b[0] * b[0] + b[1] * b[1]) - (a[0] * a[0] + a[1] * a[1]) ;
-        });
-        
-        for(int[] p : points) {
-            pq.offer(p);
-            
-            while(pq.size() > k) {
-                pq.poll();
-            }
-        }
-        
         int[][] res = new int[k][0];
+        this.points = points;
+        
+        quickSelect(0, points.length - 1, k - 1);
         
         for(int i = 0; i < k; i++) {
-            res[i] = pq.poll();
+            res[i] = points[i];
         }
         
         return res;
+    }
+    
+    void quickSelect(int start, int end, int k) {
+        int p = partition(start, end);
+        
+        if (p == k) {
+            return;
+        }
+        
+        if (p < k) {
+            quickSelect(p + 1, end, k);
+        } else {
+            quickSelect(start, p - 1, k);
+        }
+    }
+    
+    int partition(int start, int end) {
+        int v = getValue(points[start]);
+        int j = start + 1;
+        
+        for(int i = start + 1; i <= end; i++) {
+            int c = getValue(points[i]);
+            if (c < v) {
+                swap(i, j++);
+            }
+        }
+        
+        swap(start, j - 1);
+        
+        return j - 1;
+    }
+    
+    int getValue(int[] p) {
+        return p[0] * p[0] + p[1] * p[1];
+    }
+    
+    void swap(int i, int j) {
+        int[] tmp = points[i];
+        points[i] = points[j];
+        points[j] = tmp;
     }
 }
