7cd619d49d3b3cd073ce90bcc13047947c042296
Chia-Hao (Zack) Cheng <88179158+zchcheng@users.noreply.github.com>
2022-05-03 18:03:44 -0700

    Time: 8 ms (24.34%), Space: 44.3 MB (28.56%) - LeetHub

---
+++ 249-group-shifted-strings/249-group-shifted-strings.java
@@ -0,0 +1,28 @@
+class Solution {
+    public List<List<String>> groupStrings(String[] strings) {
+        Map<String, List<String>> map = new HashMap<>();
+        
+        for(String s : strings) {
+            String enc = encode(s);
+            List<String> list = map.computeIfAbsent(enc, i -> new ArrayList<>());
+            list.add(s);
+        }
+        
+        return new LinkedList<>(map.values());
+    }
+    
+    String encode(String s) {
+        if (s.isEmpty()) return "";
+        
+        int n = s.length();
+        String[] arr = new String[n];
+        char p = s.charAt(0);
+        
+        for(int i = 1; i < n; i++) {
+            char c = s.charAt(i);
+            arr[i] = String.valueOf(((c - p) + 26) % 26);
+        }
+        
+        return String.join(",", arr);
+    }
+}
