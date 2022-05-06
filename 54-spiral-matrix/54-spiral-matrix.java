346cc3b0cd3ef7535a15deec7d71326a799050d8
Chia-Hao (Zack) Cheng <88179158+zchcheng@users.noreply.github.com>
2022-05-05 18:10:15 -0700

    Time: 1 ms (10.63%), Space: 42 MB (53.26%) - LeetHub

--- 54-spiral-matrix/54-spiral-matrix.java
+++ 54-spiral-matrix/54-spiral-matrix.java
@@ -3,25 +3,25 @@ class Solution {
         List<Integer> res = new ArrayList<>();
         
         int s = 0;
-        int[][] dir = new int[][] {
-            {0, 1}, {1, 0}, {0, -1}, {-1, 0}
-        };
-        
         int r = matrix.length;
         int c = matrix[0].length;
-        int[] ep = new int[] {-1, r, -1, c};
+        int total = r * c;
+        int[][] dir = new int[][] { {0, 1}, {1, 0}, {0, -1}, {-1, 0} };
+        int[] corner = new int[] {-1, r, -1, c};
         
-        int total = r * c, i = 0, j = 0;
+        int i = 0, j = 0;
         while(res.size() < total) {
             res.add(matrix[i][j]);
             
-            if (end(ep, i + dir[s][0], j + dir[s][1])) {
+            if (end(corner, i + dir[s][0], j + dir[s][1])) {
                 s = (s + 1) % dir.length;
                 int[] p = dir[s];
-                ep[0] = Math.max(ep[0], ep[0] + p[0]);
-                ep[1] = Math.min(ep[1], ep[1] + p[0]);
-                ep[2] = Math.max(ep[2], ep[2] + p[1]);
-                ep[3] = Math.min(ep[3], ep[3] + p[1]);
+                corner = new int[] {
+                    Math.max(corner[0], corner[0] + p[0]),
+                    Math.min(corner[1], corner[1] + p[0]),
+                    Math.max(corner[2], corner[2] + p[1]),
+                    Math.min(corner[3], corner[3] + p[1])
+                };
             }
             
             i += dir[s][0];
@@ -31,7 +31,7 @@ class Solution {
         return res;
     }
     
-    boolean end(int[] ep, int i, int j) {
-        return i <= ep[0] || i >= ep[1] || j <= ep[2] || j >= ep[3];
+    boolean end(int[] corner, int i, int j) {
+        return i <= corner[0] || i >= corner[1] || j <= corner[2] || j >= corner[3];
     }
 }
