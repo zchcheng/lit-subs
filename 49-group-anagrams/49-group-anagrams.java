97805de6ff5ba5a0108459aa86eb965c606fd25f
Chia-Hao (Zack) Cheng <88179158+zchcheng@users.noreply.github.com>
2022-05-05 17:23:31 -0700

    Time: 23 ms (28.06%), Space: 60.4 MB (17.24%) - LeetHub

---
+++ 49-group-anagrams/49-group-anagrams.java
@@ -0,0 +1,37 @@
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        Map<String, List<String>> map = new HashMap<>();
+        
+        for(String s : strs) {
+            String enc = encode(s);
+            List<String> list = map.get(enc);
+            
+            if (list == null) {
+                list = new ArrayList<>();
+                map.put(enc, list);
+            }
+            
+            list.add(s);
+        }
+        
+        return new ArrayList<>(map.values());
+    }
+    
+    String encode(String str) {
+        int[] bucket = new int[26];
+        
+        for(int i = 0; i < str.length(); i++) {
+            char c = str.charAt(i);
+            bucket[c - 'a']++;
+        }
+        
+        StringBuilder sb = new StringBuilder();
+        
+        for(int i = 0; i < 26; i++) {
+            sb.append(bucket[i]);
+            sb.append(',');
+        }
+        
+        return sb.toString();
+    }
+}
