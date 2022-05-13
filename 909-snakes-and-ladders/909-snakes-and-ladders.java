3f597c0c7e3cf936d2c1b9ea987eca69d3802e55
Chia-Hao (Zack) Cheng <88179158+zchcheng@users.noreply.github.com>
2022-05-12 21:04:06 -0600

    Time: 7 ms (76.54%), Space: 42.3 MB (85.10%) - LeetHub

--- 909-snakes-and-ladders/909-snakes-and-ladders.java
+++ 909-snakes-and-ladders/909-snakes-and-ladders.java
@@ -8,29 +8,28 @@ class Solution {
         
         q.offer(new Loc(1));
         
-        int turn = 0;
+        int res = 0;
+        
         while(!q.isEmpty()) {
             for(int i = q.size(); i > 0; i--) {
                 Loc loc = q.poll();
                 
-                if (loc.dest()) return turn;
-                
                 for(int j = 0; j < 6; j++) {
-                    loc.next();
-                    Loc next = null;
+                    Loc next = loc.getNext(j + 1);
                     
-                    if (board[loc.r][loc.c] != -1) {
-                        next = new Loc(board[loc.r][loc.c]);
-                    } else {
-                        next = new Loc(loc.x);
+                    if (board[next.r][next.c] != -1) {
+                        next = new Loc(board[next.r][next.c]);
+                    } 
+                    
+                    if (next.dest()) return res + 1;
+                    
+                    if (!visited.contains(next.x)) { 
+                        visited.add(next.x); 
+                        q.offer(next); 
                     }
-                    
-                    if (next.dest()) return turn + 1;
-                    
-                    if (!visited.contains(next.x)) { visited.add(next.x); q.offer(next); }
                 }
             }
-            turn++;
+            res++;
         }
         
         return -1;
@@ -42,22 +41,6 @@ class Solution {
         public int x;
         
         public Loc(int x) {
-            set(x);
-        }
-        
-        public Loc(int r, int c) {
-            this.r = r;
-            this.c = c;
-            this.x = n * (n - r - 1);
-            if ((n - r - 1) % 2 == 0) this.x += c + 1;
-            else this.x += n - c;
-        }
-        
-        public void next() {
-            set(x + 1);
-        }
-        
-        void set(int x) {
             r = n - ((x - 1) / n) - 1;
             c = (x - 1) % n;
             if ((n - r - 1) % 2 == 1) c = n - c - 1;
@@ -68,15 +51,8 @@ class Solution {
             return x == n * n;
         }
         
-        @Override
-        public boolean equals(Object o) {
-            Loc l = (Loc)o;
-            return l.x == x;
-        }
-        
-        @Override
-        public int hashCode() {
-            return x;
+        public Loc getNext(int i) {
+            return new Loc(x + i);
         }
     }
 }
