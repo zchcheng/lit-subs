4caf95de1ea63439a8e5c79062abea3c83e04657
Chia-Hao (Zack) Cheng <88179158+zchcheng@users.noreply.github.com>
2022-05-14 00:51:21 -0600

    Time: 31 ms (86.30%), Space: 53.9 MB (77.00%) - LeetHub

--- 1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit.java
+++ 1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit.java
@@ -1,28 +1,21 @@
 class Solution {
     public int longestSubarray(int[] nums, int limit) {
-        PriorityQueue<int[]> min = new PriorityQueue<>((int[] a, int[] b) -> {
-            return a[0] - b[0];
-        });
-        PriorityQueue<int[]> max = new PriorityQueue<>((int[] a, int[] b) -> {
-            return b[0] - a[0];
-        });
-        
+        Deque<Integer> min = new LinkedList<>();
+        Deque<Integer> max = new LinkedList<>();
         int res = 0;
         
         for(int right = 0, left = -1; right < nums.length; right++) {
             int v = nums[right];
-            int[] p = new int[] {v, right};
-            
-            min.offer(p);
-            max.offer(p);
+            while(!min.isEmpty() && min.peekFirst() > v) min.pop();
+            while(!max.isEmpty() && max.peekFirst() < v) max.pop();
+            min.push(v);
+            max.push(v);
             
             while(left < right) {
-                if (max.peek()[0] - min.peek()[0] <= limit)  {
-                    break;
-                }
+                if (max.peekLast() - min.peekLast() <= limit)  break;
                 left++;
-                while(max.peek()[1] <= left) max.poll();
-                while(min.peek()[1] <= left) min.poll();
+                if (nums[left] == max.peekLast()) max.pollLast();
+                if (nums[left] == min.peekLast()) min.pollLast();
             }
             
             res = Math.max(res, right - left);
