6089192a5d98a2dde421e4c0801160c165cc83b1
Chia-Hao (Zack) Cheng <88179158+zchcheng@users.noreply.github.com>
2022-05-29 20:15:49 -0600

    Time: 912 ms (25.00%), Space: 70.7 MB (25.63%) - LeetHub

--- 21-merge-two-sorted-lists/21-merge-two-sorted-lists.scala
+++ 21-merge-two-sorted-lists/21-merge-two-sorted-lists.scala
@@ -11,19 +11,21 @@ object Solution {
         var cur = res
         var h1 = list1
         var h2 = list2
-
-        while (h1 != null || h2 != null) {
-            val v1 = if (h1 == null) Int.MaxValue else h1.x
-            val v2 = if (h2 == null) Int.MaxValue else h2.x
-
-            cur.next = ListNode(v1 min v2)
+        
+        while(h1 != null || h2 != null) {
+            val v1 = if (h1 == null) Integer.MAX_VALUE else h1.x
+            val v2 = if (h2 == null) Integer.MAX_VALUE else h2.x
             
-            if (cur.next.x == v1) h1 = h1.next
-            else h2 = h2.next
-
+            cur.next = ListNode(v1 min v2)
             cur = cur.next
+            
+            if (v1 <= v2) {
+                h1 = h1.next
+            } else {
+                h2 = h2.next
+            }
         }
-
+        
         res.next
     }
 }
