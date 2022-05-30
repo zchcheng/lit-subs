928c3df9961a698a2ef0b5911ec04119492b7ea0
Chia-Hao (Zack) Cheng <88179158+zchcheng@users.noreply.github.com>
2022-05-30 17:13:59 -0600

    Time: 942 ms (37.16%), Space: 71 MB (48.96%) - LeetHub

--- 13-roman-to-integer/13-roman-to-integer.scala
+++ 13-roman-to-integer/13-roman-to-integer.scala
@@ -1,9 +1,21 @@
+import scala.collection.mutable._
+
 object Solution {
     def romanToInt(s: String): Int = {
-        val map: Map[Char, Int] = Map('I'->1, 'V'->5, 'X'->10, 'L'->50, 'C'->100, 'D'->500, 'M'->1000)
-        val vals = s.map(map)
-        vals.zipWithIndex.map{ case (e, i) => 
-            if (i < vals.length - 1 && (vals(i + 1) == 5 * e || vals(i + 1) == 10 * e)) -e else e
-        }.reduce(_ + _)
+        val map = Map({'I' -> 1}, {'V' -> 5}, {'X' -> 10}, {'L' -> 50}, {'C' -> 100}, {'D' -> 500}, {'M' -> 1000})
+        var res = 0
+        
+        for(i <- 0 to s.length - 1) {
+            val prev = if (i == 0) 0 else map(s.charAt(i - 1))
+            val cur = map(s.charAt(i))
+            
+            if (prev * 5 == cur || prev * 10 == cur) {
+                res -= 2 * prev
+            }
+            
+            res += cur
+        }
+        
+        res
     }
 }
