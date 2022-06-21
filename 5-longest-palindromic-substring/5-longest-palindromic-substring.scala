433b0279f020a30e0fedca5709107376bf635138
Chia-Hao (Zack) Cheng <88179158+zchcheng@users.noreply.github.com>
2022-06-21 01:24:50 -0600

    Time: 1856 ms (30.14%), Space: 117.6 MB (41.55%) - LeetHub

--- 5-longest-palindromic-substring/5-longest-palindromic-substring.scala
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.scala
@@ -7,20 +7,10 @@ object Solution {
       Array.fill(s.length)(false)
     }
 
-    for (i <- 0 to s.length - 1) dp(0)(i) = true
-
-    for (i <- 1 to s.length - 1) {
-      if (s.charAt(i) == s.charAt(i - 1)) {
-        dp(1)(i) = true
-        ml = 2
-        res = s.substring(i - 1, i + 1)
-      }
-    }
-
-    for (i <- 2 to s.length - 1) {
+    for (i <- 0 to s.length - 1) {
       for (j <- 0 to s.length - 1) {
         dp(i)(j) =
-          (j - i >= 0 && dp(i - 2)(j - 1) && s.charAt(j) == s.charAt(j - i))
+          (j - i >= 0 && (i < 2 || dp(i - 2)(j - 1)) && s.charAt(j) == s.charAt(j - i))
         if (dp(i)(j) && i >= ml) {
           ml = i + 1
           res = s.substring(j - i, j + 1)
