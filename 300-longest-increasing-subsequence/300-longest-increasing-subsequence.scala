6536f0db7240b834ccd0ec8fa5d7a1c23256b010
Chia-Hao (Zack) Cheng <88179158+zchcheng@users.noreply.github.com>
2022-06-26 17:13:03 -0600

    Time: 985 ms (84.00%), Space: 73.1 MB (78.00%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.scala
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.scala
@@ -5,14 +5,30 @@ object Solution {
         val list: ListBuffer[Int] = ListBuffer()
 
         nums.foreach { num => 
-            var i = list.length - 1
+            val i = binSearch(list, num)
 
-            while (i >= 0 && list(i) >= num) i -= 1
-            
-            if (i == list.length - 1) list += num
-            else list(i + 1) = num
+            if (i == list.length) list += num
+            else list(i) = num
         }
 
         list.length
+    }
+
+    def binSearch(list: ListBuffer[Int], target: Int): Int = {
+        var s = 0
+        var e = list.length - 1
+
+        while (s <= e) {
+            val m = (s + e) / 2
+            val v = list(m)
+
+            if (v >= target) {
+                e = m - 1
+            } else {
+                s = m + 1
+            }
+        }
+
+        s
     }
 }
