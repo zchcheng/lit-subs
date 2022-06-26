ed7c4952ca93fb8875a0b6028939b6c12c577f65
Chia-Hao (Zack) Cheng <88179158+zchcheng@users.noreply.github.com>
2022-06-26 17:02:38 -0600

    Time: 1108 ms (70.00%), Space: 73.8 MB (66.00%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.scala
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.scala
@@ -2,21 +2,17 @@ import scala.collection.mutable._
 
 object Solution {
     def lengthOfLIS(nums: Array[Int]): Int = {
-        val dp: Array[Int] = Array.fill(nums.length)(1)
-        var res = 0
+        val list: ListBuffer[Int] = ListBuffer()
 
-        for(i <- 0 to nums.length - 1) {
-            var tmp = 1
-            val v = nums(i)
+        nums.foreach { num => 
+            var i = list.length - 1
 
-            for(j <- 0 to i) {
-                if (nums(j) < v) tmp = tmp max (1 + dp(j))
-            }
-
-            dp(i) = tmp
-            res = res max tmp
+            while (i >= 0 && list(i) >= num) i -= 1
+            
+            if (i == list.length - 1) list += num
+            else list(i + 1) = num
         }
 
-        res
+        list.length
     }
 }
