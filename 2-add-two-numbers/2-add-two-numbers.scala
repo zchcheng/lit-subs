b03f5910f1f6ff92ee3edbfe3747372d712b4fc7
Chia-Hao (Zack) Cheng <88179158+zchcheng@users.noreply.github.com>
2022-07-03 20:52:55 -0600

    Time: 1441 ms (5.01%), Space: 79.2 MB (36.82%) - LeetHub

--- 2-add-two-numbers/2-add-two-numbers.scala
+++ 2-add-two-numbers/2-add-two-numbers.scala
@@ -1,32 +1,33 @@
-/**
- * Definition for singly-linked list.
- * class ListNode(_x: Int = 0, _next: ListNode = null) {
- *   var next: ListNode = _next
- *   var x: Int = _x
- * }
- */
 object Solution {
     def addTwoNumbers(l1: ListNode, l2: ListNode): ListNode = {
-        var h1 = l1
-        var h2 = l2
+        var i = l1
+        var j = l2
         var c = 0
-        var res = ListNode(0)
-        var h = res
-        
-        while(h1 != null || h2 != null) {
-            val v1 = if (h1 == null) 0 else h1.x
-            val v2 = if (h2 == null) 0 else h2.x
-            val sum = (v1 + v2 +c)
-            h.next = ListNode(sum % 10)
-            h = h.next
-            c = sum / 10
-            
-            if (h1 != null) h1 = h1.next
-            if (h2 != null) h2 = h2.next
+
+        val res = ListNode()
+        var head = res
+
+        while(i != null || j != null) {
+            val vi = if (i != null) {
+                val v = i.x
+                i = i.next
+                v
+            } else 0
+
+            val vj = if (j != null) {
+                val v = j.x
+                j = j.next
+                v
+            } else 0
+
+            head.next = ListNode((vi + vj + c) % 10)
+            c = (vi + vj + c) / 10
+
+            head = head.next
         }
-        
-        if (c != 0) h.next = ListNode(c)
-        
+
+        if (c != 0) head.next = ListNode(c)
+
         res.next
     }
 }
