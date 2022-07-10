0c6896ed78319ebfd6421ac154f2a931d5e6d148
Chia-Hao (Zack) Cheng <88179158+zchcheng@users.noreply.github.com>
2022-07-09 18:40:11 -0600

    Time: 2510 ms (37.50%), Space: 97.2 MB (75.00%) - LeetHub

---
+++ 127-word-ladder/127-word-ladder.scala
@@ -0,0 +1,67 @@
+
+import scala.collection.mutable._
+
+object Solution {
+    def ladderLength(beginWord: String, endWord: String, wordList: List[String]): Int = {
+        val graph: Map[String, Set[String]] = Map()
+
+        (wordList :+ beginWord).foreach { w1 =>
+            val set: Set[String] = Set()
+
+            wordList.foreach { w2 =>
+                if (w1 != w2 && isConnected(w1, w2)) {
+                    set += w2
+                }
+            }
+
+            graph += (w1 -> set)
+        }
+        
+        println(graph)
+
+        val visited: Set[String] = Set()
+        val queue: Queue[String] = Queue()
+
+        queue.enqueue(beginWord)
+        visited += beginWord
+
+        var res = 0
+        var found = false
+
+        while(!found && queue.nonEmpty) {
+            val size = queue.size
+
+            for(i <- 1 to size) {
+                val word = queue.dequeue
+
+                if (word == endWord) { found = true }
+                else {
+                    val connected = graph.get(word).getOrElse(Set())
+                    println("connected: " + connected)
+                    connected.foreach { other =>
+                        if (!visited.contains(other)) {
+                            queue.enqueue(other)
+                            visited += other
+                        }
+                    }
+                }
+            }
+
+            res += 1
+        }
+
+        if (found) res else 0
+    }
+
+    def isConnected(w1: String, w2: String): Boolean = {
+        if (w1.length == w2.length) {
+            var diff = 0
+
+            for(i <- 0 to w1.length - 1) {
+                diff += (if (w1.charAt(i) != w2.charAt(i)) 1 else 0)
+            }
+
+            diff == 1
+        } else false
+    }
+}
