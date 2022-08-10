ae4f6cc2e7461db853f0faaa7c492b5fb0e00cfa
Chia-Hao (Zack) Cheng <88179158+zchcheng@users.noreply.github.com>
2022-08-09 22:58:55 -0700

    Time: 9 ms (50.72%), Space: 44.4 MB (79.44%) - LeetHub

---
+++ 252-meeting-rooms/252-meeting-rooms.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean canAttendMeetings(int[][] intervals) {
+        Arrays.sort(intervals, (a, b) -> {
+            return a[0] - b[0];
+        });
+        
+        for(int i = 1; i < intervals.length; i++) {
+            if (intervals[i][0] >= intervals[i - 1][1]) {
+                continue;
+            }
+            
+            return false;
+        }
+        
+        return true;
+    }
+}
