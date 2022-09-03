b84f70cba1b7f2e37e665ae36ef10ecc080f3c5d
Chia-Hao (Zack) Cheng <88179158+zchcheng@users.noreply.github.com>
2022-09-02 21:28:27 -0700

    Time: 2 ms (18.21%), Space: 42.7 MB (64.29%) - LeetHub

--- 50-powx-n/50-powx-n.java
+++ 50-powx-n/50-powx-n.java
@@ -1,13 +1,17 @@
 class Solution {
     public double myPow(double x, int n) {
-        return helper(x, n);
-    }
-    
-    double helper(double x, long n) {
-        if (n == 0) return 1.0;
-        if (n == 1) return x;
-        if (n < 0) return helper(1 / x, -n);
-        double half = helper(x, n / 2);
-        return half * half * ((n % 2 == 0)? 1 : x);
+        x = (n >= 0)? x : 1 / x;
+        long N = n;
+        N = (N >= 0)? N : -N;
+        
+        double res = 1;
+        double cur = x;
+        
+        for(long i = N; i > 0; i >>= 1) {
+            if (i % 2 != 0) res = res * cur;
+            cur = cur * cur;
+        }
+        
+        return res;
     }
 }
