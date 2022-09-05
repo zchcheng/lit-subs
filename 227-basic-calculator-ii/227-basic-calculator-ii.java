0e52a4739010a85b98443feeb7cad58cc3719f86
Chia-Hao (Zack) Cheng <88179158+zchcheng@users.noreply.github.com>
2022-09-04 22:26:01 -0700

    Time: 18 ms (69.67%), Space: 46.3 MB (21.32%) - LeetHub

--- 227-basic-calculator-ii/227-basic-calculator-ii.java
+++ 227-basic-calculator-ii/227-basic-calculator-ii.java
@@ -1,39 +1,29 @@
 class Solution {
     public int calculate(String s) {
         Deque<Integer> stack = new LinkedList<>();
-        
         int cur = 0;
         Character op = '+';
-        for(char c : s.toCharArray()) {
-            if (c == ' ') continue;
+        
+        for(int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
             
             if (Character.isDigit(c)) {
                 cur = cur * 10 + Character.getNumericValue(c);
-                continue;
             }
             
-            if (op == '*') {
-                stack.push(stack.pop() * cur);
-            } else if (op == '/') {
-                stack.push(stack.pop() / cur);
-            } else if (op == '-') {
-                stack.push(-cur);
-            } else {
-                stack.push(cur);
+            if ((c != ' ' && !Character.isDigit(c)) || i == s.length() - 1) {
+                if (op == '*') {
+                    stack.push(stack.pop() * cur);
+                } else if (op == '/') {
+                    stack.push(stack.pop() / cur);
+                } else if (op == '-') {
+                    stack.push(-cur);
+                } else {
+                    stack.push(cur);
+                }
+                op = c;
+                cur = 0;
             }
-            
-            op = c;
-            cur = 0;
-        }
-        
-        if (op == '*') {
-            stack.push(stack.pop() * cur);
-        } else if (op == '/') {
-            stack.push(stack.pop() / cur);
-        } else if (op == '-') {
-            stack.push(-cur);
-        } else {
-            stack.push(cur);
         }
         
         int res = 0;
