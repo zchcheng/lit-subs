96e2e2dafd035c0744d564c5025db77067b9b8a0
Chia-Hao (Zack) Cheng <88179158+zchcheng@users.noreply.github.com>
2022-09-06 12:07:33 -0700

    Time: 1 ms (99.77%), Space: 48.3 MB (76.31%) - LeetHub

--- 42-trapping-rain-water/42-trapping-rain-water.java
+++ 42-trapping-rain-water/42-trapping-rain-water.java
@@ -1,19 +1,23 @@
 class Solution {
     public int trap(int[] height) {
         int res = 0;
+        int l = 0;
+        int r = height.length - 1;
+        int ml = 0;
+        int mr = 0;
         
-        Deque<int[]> stack = new LinkedList<>();
-        
-        for(int i = 0; i < height.length; i++) {
-            int h = height[i];
-            
-            while(!stack.isEmpty() && stack.peek()[0] < h) {
-                int[] prev = stack.pop();
-                if (stack.isEmpty()) continue;
-                res += (Math.min(stack.peek()[0], h) - prev[0]) * (i - stack.peek()[1] - 1);
+        while(l < r) {
+            if (height[l] < height[r]) {
+                ml = Math.max(height[l], ml);
+                res += ml - height[l];
+                l++;
             }
             
-            stack.push(new int[] {h, i});
+            if (height[l] >= height[r]) {
+                mr = Math.max(height[r], mr);
+                res += mr - height[r];
+                r--;
+            }
         }
         
         return res;
