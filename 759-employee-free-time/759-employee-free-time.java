edf29a08df926ac46671de037520545d7a5cbc41
Chia-Hao (Zack) Cheng <88179158+zchcheng@users.noreply.github.com>
2022-09-07 13:42:13 -0700

    Time: 20 ms (46.66%), Space: 55.2 MB (23.89%) - LeetHub

--- 759-employee-free-time/759-employee-free-time.java
+++ 759-employee-free-time/759-employee-free-time.java
@@ -15,73 +15,33 @@ class Interval {
 
 class Solution {
     public List<Interval> employeeFreeTime(List<List<Interval>> schedule) {
-        List<Interval> result = List.of(new Interval(Integer.MIN_VALUE, Integer.MAX_VALUE));
+        List<Interval> allIntervals = new ArrayList<>();
         
-        for(List<Interval> intervals : schedule) {
-            List<Interval> next = new LinkedList<>();
-            List<Interval> free = getFreeIntervals(intervals);
-            
-            for(int i = 0, j = 0; i < result.size() && j < free.size();) {
-                Interval cmn = result.get(i);
-                Interval emp = free.get(j);
-                
-                if (cmn.end <= emp.start) {
-                    i++;
-                    continue;
-                }
-                
-                if (emp.end <= cmn.start) {
-                    j++;
-                    continue;
-                }
-                
-                Interval ni = new Interval(
-                    Math.max(cmn.start, emp.start),
-                    Math.min(cmn.end, emp.end)
-                );
-                
-                if (ni.start >= ni.end) continue;
-                
-                next.add(ni);
-                
-                if (cmn.end > emp.end) j++;
-                else i++;
-            }
-                
-            result = next;
+        for(List<Interval> intvs : schedule) {
+            allIntervals.addAll(intvs);
         }
         
-        List<Interval> finals = new LinkedList<>();
+        allIntervals.sort((a, b) -> a.start - b.start);
         
-        for(Interval intv : result) {
-            if (intv.start != Integer.MIN_VALUE && intv.end != Integer.MAX_VALUE) finals.add(intv);
-        }
-        
-        return finals;
-    }
-    
-    List<Interval> getFreeIntervals(List<Interval> intervals) {
         List<Interval> result = new ArrayList<>();
         
-        result.add(new Interval(
-            Integer.MIN_VALUE,
-            intervals.get(0).start
-        ));
+        Interval current = allIntervals.get(0);
         
-        for(int i = 1; i < intervals.size(); i++) {
-            Interval ni = new Interval(
-                intervals.get(i - 1).end,
-                intervals.get(i).start
-            );
-            if (ni.start >= ni.end) continue;
-            result.add(ni);
+        for(int i = 1; i < allIntervals.size(); i++) {
+            Interval itv = allIntervals.get(i);
+            
+            if (current.end < itv.start) {
+                result.add(new Interval(
+                    current.end,
+                    itv.start
+                ));
+            }
+            
+            if (itv.end >= current.end) {
+                current = itv;
+            }
         }
         
-        result.add(new Interval(
-            intervals.get(intervals.size() - 1).end,
-            Integer.MAX_VALUE
-        ));
-            
         return result;
     }
 }
