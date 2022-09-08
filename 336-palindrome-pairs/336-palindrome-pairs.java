576696d6c8e0f58892f370fcc0fafc2131bc3f31
Chia-Hao (Zack) Cheng <88179158+zchcheng@users.noreply.github.com>
2022-09-08 16:29:57 -0700

    Time: 1030 ms (69.80%), Space: 359.2 MB (12.24%) - LeetHub

--- 336-palindrome-pairs/336-palindrome-pairs.java
+++ 336-palindrome-pairs/336-palindrome-pairs.java
@@ -1,15 +1,10 @@
 class TrieNode {
-    public int wordEnding = -1; // We'll use -1 to mean there's no word ending here.
+    public int wordEnding = -1;
     public Map<Character, TrieNode> next = new HashMap<>();
     public List<Integer> palindromePrefixRemaining = new ArrayList<>();
 }
 
 class Solution {
-
-    // Is the given string a palindrome after index i?
-    // Tip: Leave this as a method stub in an interview unless you have time
-    // or the interviewer tells you to write it. The Trie itself should be
-    // the main focus of your time.
     public boolean hasPalindromeRemaining(String s, int i) {
         int p1 = i;
         int p2 = s.length() - 1;
@@ -21,51 +16,56 @@ class Solution {
     }
 
     public List<List<Integer>> palindromePairs(String[] words) {
-
-        TrieNode trie = new TrieNode();
+        TrieNode root = new TrieNode();
 
         // Build the Trie
         for (int wordId = 0; wordId < words.length; wordId++) {
+            
             String word = words[wordId];
             String reversedWord = new StringBuilder(word).reverse().toString();
-            TrieNode currentTrieLevel = trie;
+            
+            TrieNode curNode = root;
+            
             for (int j = 0; j < word.length(); j++) {
+                
                 if (hasPalindromeRemaining(reversedWord, j)) {
-                    currentTrieLevel.palindromePrefixRemaining.add(wordId);
+                    curNode.palindromePrefixRemaining.add(wordId);
                 }
-                Character c = reversedWord.charAt(j);
-                if (!currentTrieLevel.next.containsKey(c)) {
-                    currentTrieLevel.next.put(c, new TrieNode());
-                }
-                currentTrieLevel = currentTrieLevel.next.get(c);
+                
+                char c = reversedWord.charAt(j);
+                curNode = curNode.next.computeIfAbsent(c, k -> new TrieNode());
             }
-            currentTrieLevel.wordEnding = wordId;
+            
+            curNode.wordEnding = wordId;
         }
 
         // Find pairs
         List<List<Integer>> pairs = new ArrayList<>();
+        
         for (int wordId = 0; wordId < words.length; wordId++) {
             String word = words[wordId];
-            TrieNode currentTrieLevel = trie;
+            TrieNode curNode = root;
+            
             for (int j = 0; j < word.length(); j++) {
-                // Check for pairs of case 3.
-                if (currentTrieLevel.wordEnding != -1
+                if (curNode.wordEnding != -1
                    && hasPalindromeRemaining(word, j)) {
-                    pairs.add(Arrays.asList(wordId, currentTrieLevel.wordEnding));
+                    pairs.add(List.of(wordId, curNode.wordEnding));
                 }
-                // Move down to the next trie level.
-                Character c = word.charAt(j);
-                currentTrieLevel = currentTrieLevel.next.get(c);
-                if (currentTrieLevel == null) break;
+                
+                char c = word.charAt(j);
+                curNode = curNode.next.get(c);
+                if (curNode == null) break;
+                
             }
-            if (currentTrieLevel == null) continue;
-            // Check for pairs of case 1. Note the check to prevent non distinct pairs.
-            if (currentTrieLevel.wordEnding != -1 && currentTrieLevel.wordEnding != wordId) {
-                pairs.add(Arrays.asList(wordId, currentTrieLevel.wordEnding));
+            
+            if (curNode == null) continue;
+            
+            if (curNode.wordEnding != -1 && curNode.wordEnding != wordId) {
+                pairs.add(List.of(wordId, curNode.wordEnding));
             }
-            // Check for pairs of case 2.
-            for (int other : currentTrieLevel.palindromePrefixRemaining) {
-                pairs.add(Arrays.asList(wordId, other));
+            
+            for (int other : curNode.palindromePrefixRemaining) {
+                pairs.add(List.of(wordId, other));
             }
         }
 
