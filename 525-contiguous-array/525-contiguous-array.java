e5b657be83e8ae513739cb8ebc08142e1227fe4a
Chia-Hao (Zack) Cheng <88179158+zchcheng@users.noreply.github.com>
2022-09-09 16:17:00 -0700

    Time: 26 ms (88.03%), Space: 51.8 MB (88.07%) - LeetHub

--- 525-contiguous-array/525-contiguous-array.java
+++ 525-contiguous-array/525-contiguous-array.java
@@ -1,19 +1,14 @@
 class Solution {
     public int findMaxLength(int[] nums) {
+        int res = 0, balance = 0;
         Map<Integer, Integer> map = new HashMap<>();
-        int res = 0;
+        map.put(balance, -1);
         
-        map.put(0, -1);
-        
-        for(int i = 0, cur = 0; i < nums.length; i++) {
-            if (nums[i] == 0) cur--;
-            else cur++;
-            
-            if (map.containsKey(cur)) {
-                res = Math.max(res, i - map.get(cur));
-            } else {
-                map.put(cur, i);
-            }
+        for(int i = 0; i < nums.length; i++) {
+            int n = nums[i];
+            balance += (n == 0)? -1 : 1;
+            if (!map.containsKey(balance)) map.put(balance, i);
+            res = Math.max(res, i - map.get(balance));
         }
         
         return res;
