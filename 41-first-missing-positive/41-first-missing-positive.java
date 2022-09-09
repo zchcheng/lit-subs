b8e18221387ab1e8e818adbf0693999e1a7d571a
Chia-Hao (Zack) Cheng <88179158+zchcheng@users.noreply.github.com>
2022-09-08 20:44:30 -0700

    Time: 2 ms (92.42%), Space: 57 MB (88.15%) - LeetHub

---
+++ 41-first-missing-positive/41-first-missing-positive.java
@@ -0,0 +1,35 @@
+class Solution {
+    public int firstMissingPositive(int[] nums) {
+        for(int i = 0; i < nums.length; i++) {
+            if (nums[i] == Integer.MAX_VALUE) nums[i] = -1;
+        }
+        
+        for(int i = 0; i < nums.length; i++) {
+            if (nums[i] > 0) {
+                helper(nums, nums[i]);
+            }
+        }
+        
+        for(int i = 0; i < nums.length; i++) {
+            if (nums[i] != Integer.MAX_VALUE) return i + 1;
+        }
+        
+        return nums.length + 1;
+    }
+    
+    void helper(int[] arr, int n) {
+        if (n <= 0) return;
+        if (n > arr.length) return;
+        
+        if (arr[n - 1] == 0) {
+            arr[n - 1] = Integer.MAX_VALUE;
+            return;
+        }
+        
+        if (arr[n - 1] != Integer.MAX_VALUE) {
+            int v = arr[n - 1];
+            arr[n - 1] = Integer.MAX_VALUE;
+            helper(arr, v);
+        }
+    }
+}
