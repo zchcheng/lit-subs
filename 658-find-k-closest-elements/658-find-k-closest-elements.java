b050cc2b47e1ba38b59b6721bf98b0c67a525939
Chia-Hao (Zack) Cheng <88179158+zchcheng@users.noreply.github.com>
2022-09-09 17:00:07 -0700

    Time: 3 ms (99.84%), Space: 44 MB (96.20%) - LeetHub

--- 658-find-k-closest-elements/658-find-k-closest-elements.java
+++ 658-find-k-closest-elements/658-find-k-closest-elements.java
@@ -1,23 +1,42 @@
 class Solution {
     public List<Integer> findClosestElements(int[] arr, int k, int x) {
+        int idx = binSearch(arr, x);
+        
+        int r = idx;
+        int l = idx - 1;
+        
         List<Integer> res = new ArrayList<>();
         
-        int l = 0;
-        int r = arr.length - k - 1;
-        
-        while(l <= r) {
-            int m = l + (r - l) / 2;
-            if (x - arr[m] > arr[m + k] - x) {
-                l = m + 1;
-            } else {
-                r = m - 1;
+        for(int i = 0; i < k; i++) {
+            if (r >= arr.length) {
+                l--;
+                continue;
+            } else if (l < 0) {
+                r++;
+                continue;
             }
+            
+            if (Math.abs(arr[r] - x) < Math.abs(x - arr[l])) r++;
+            else l--;
         }
         
-        for(int i = l; i < l + k; i++) {
-            res.add(arr[i]);
+        for(int i = 0; i < k; i++) {
+            res.add(arr[i + l + 1]);
         }
         
         return res;
+    }
+    
+    int binSearch(int[] arr, int x) {
+        int s = 0;
+        int e = arr.length - 1;
+        
+        while(s <= e) {
+            int m = s + (e - s) / 2;
+            if (arr[m] < x) s = m + 1;
+            else e = m - 1;
+        }
+        
+        return s;
     }
 }
