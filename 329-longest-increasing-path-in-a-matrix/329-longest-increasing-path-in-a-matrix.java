ad738cced5dbe07a0c2f906621202a4e87fab713
Chia-Hao (Zack) Cheng <88179158+zchcheng@users.noreply.github.com>
2022-09-13 00:48:35 -0700

    Time: 53 ms (12.27%), Space: 73.1 MB (5.19%) - LeetHub

--- 329-longest-increasing-path-in-a-matrix/329-longest-increasing-path-in-a-matrix.java
+++ 329-longest-increasing-path-in-a-matrix/329-longest-increasing-path-in-a-matrix.java
@@ -1,43 +1,39 @@
 class Solution {
+    Map<Pair<Integer, Integer>, Integer> memo = new HashMap<>();
+    
     public int longestIncreasingPath(int[][] matrix) {
-        int r = matrix.length;
-        int c = matrix[0].length;
-        
-        int[][] memo = new int[r][c];
-        
-        for(int i = 0; i < r; i++) {
-            for(int j = 0; j < c; j++) {
-                memo[i][j] = -1;
-            }
-        }
-        
         int res = 0;
         
-        for(int i = 0; i < r; i++) {
-            for(int j = 0; j < c; j++) {
-                res = Math.max(res, helper(matrix, i, j, memo));
+        for(int i = 0; i < matrix.length; i++) {
+            for(int j = 0; j < matrix[0].length; j++) {
+                res = Math.max(res, helper(matrix, new int[] { i, j }));
             }
         }
         
         return res;
     }
     
-    int helper(int[][] m, int x, int y, int[][] memo) {
-        if (memo[x][y] != -1) return memo[x][y];
+    int helper(int[][] matrix, int[] loc) {
+        Pair<Integer, Integer> p = new Pair(loc[0], loc[1]);
         
-        int res = 1;
+        Integer res = memo.get(p);
         
-        for(int[] dir : new int[][] {{1, 0}, {-1, 0}, {0, 1}, {0, -1}}) {
-            int nx = x + dir[0];
-            int ny = y + dir[1];
+        if (res != null) return res;
+        
+        res = 1;
+        for(int[] n : new int[][] {
+            {1, 0}, {-1, 0}, {0, 1}, {0, -1}
+        }) {
+            int nx = loc[0] + n[0];
+            int ny = loc[1] + n[1];
             
-            if (nx < 0 || nx >= m.length || ny < 0 || ny >= m[0].length) continue;
-            if (m[nx][ny] <= m[x][y]) continue;
+            if (nx < 0 || ny < 0 || nx >= matrix.length || ny >= matrix[0].length) continue;
+            if (matrix[nx][ny] >= matrix[loc[0]][loc[1]]) continue;
             
-            res = Math.max(res, 1 + helper(m, nx, ny, memo));
+            res = Math.max(1 + helper(matrix, new int[] {nx, ny}), res);
         }
         
-        memo[x][y] = res;
+        memo.put(p, res);
         
         return res;
     }
