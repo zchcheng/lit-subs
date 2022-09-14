968692798c129fbe49e51e63f1ffeb0f483f77cc
Chia-Hao (Zack) Cheng <88179158+zchcheng@users.noreply.github.com>
2022-09-13 22:35:40 -0700

    Time: 0 ms (100.00%), Space: 42.8 MB (98.38%) - LeetHub

--- 268-missing-number/268-missing-number.java
+++ 268-missing-number/268-missing-number.java
@@ -1,20 +1,11 @@
 class Solution {
     public int missingNumber(int[] nums) {
-        for(int i = 0; i < nums.length; i++) {
-            int k = Math.abs(nums[i]);
-            if (k == nums.length + 1) {
-                nums[0] = -Math.abs(nums[0]);
-                continue;
-            }
-            if (k == nums.length) continue;
-            if (nums[k] == 0) nums[k] = -(nums.length + 1);
-            else nums[k] = -(Math.abs(nums[k]));
-        }
+        int res = nums.length;
         
         for(int i = 0; i < nums.length; i++) {
-            if (nums[i] >= 0) return i;
+            res = res ^ i ^ nums[i];
         }
         
-        return nums.length;
+        return res;
     }
 }
