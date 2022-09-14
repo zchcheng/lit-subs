769a898ab90bf9adb1bc21044e32e9e8d63a8c63
Chia-Hao (Zack) Cheng <88179158+zchcheng@users.noreply.github.com>
2022-09-13 23:30:51 -0700

    Time: 2 ms (78.27%), Space: 40.8 MB (81.68%) - LeetHub

--- 7-reverse-integer/7-reverse-integer.java
+++ 7-reverse-integer/7-reverse-integer.java
@@ -1,19 +1,22 @@
 class Solution {
+    final int MAX = Integer.MAX_VALUE / 10;
+    final int MAX_K = Integer.MAX_VALUE % 10;
+    
     public int reverse(int x) {
-        int k = x;
+        if (x == Integer.MIN_VALUE) return 0;
+        
         int res = 0;
+        boolean isNegative = x < 0;
+        x = Math.abs(x);
         
-        // res * 10 + (k % 10) > Integer.MAX
-        // res * 10 > Integer.MAX - (k % 10)
-        // res >
-        
-        while(k != 0) {
-            if (x > 0 && (res > Integer.MAX_VALUE / 10 || (res == Integer.MAX_VALUE / 10 && k % 10 > 7))) return 0;
-            if (x < 0 && (res < Integer.MIN_VALUE / 10 || (res == Integer.MIN_VALUE / 10 && k % 10 < -8))) return 0;
-            res = res * 10 + k % 10;
-            if (res > 0 && x < 0) res = -res;
-            k /= 10;
+        while(x > 0) {
+            int k = x % 10;
+            if (res > MAX || (res == MAX && k > MAX_K)) return 0;
+            res = res * 10 + k;
+            x = x / 10;
         }
+        
+        if (isNegative) res = -res;
         
         return res;
     }
