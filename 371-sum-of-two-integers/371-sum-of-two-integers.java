027156395cecdadb9b4aef2bd831eced0711c3ce
Chia-Hao (Zack) Cheng <88179158+zchcheng@users.noreply.github.com>
2022-09-13 23:06:59 -0700

    Time: 1 ms (100.00%), Space: 41.3 MB (20.45%) - LeetHub

--- 371-sum-of-two-integers/371-sum-of-two-integers.java
+++ 371-sum-of-two-integers/371-sum-of-two-integers.java
@@ -1,7 +1,5 @@
 class Solution {
     public int getSum(int a, int b) {
-        // ^ & <<
-        
         int res = 0;
         int mask = 1;
         int c = 0;
@@ -11,8 +9,6 @@ class Solution {
             int mb = ((mask & b) != 0)? 1 : 0;
             
             res = res | (((ma ^ mb ^ c) != 0)? mask : 0);
-            
-            //System.out.println("ma: " + ma + ", mb: " + mb + ", c: " + c + ", res: " + res);
             
             c = ((ma == 1 && mb == 1) || (ma == 1 && c == 1) || (mb == 1 && c == 1))? 1 : 0;
             
