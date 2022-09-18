42227616d576509e1e2b1fa70d7f4bdca0c26f48
Chia-Hao (Zack) Cheng <88179158+zchcheng@users.noreply.github.com>
2022-09-17 20:23:59 -0700

    Time: 2 ms, Memory: 43.9 MB - LeetHub

---
+++ binary-tree-level-order-traversal/binary-tree-level-order-traversal.java
@@ -0,0 +1,40 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> result = new ArrayList<>();
+        
+        Queue<TreeNode> queue = new LinkedList<>();
+        
+        if (root != null) queue.offer(root);
+        
+        while(!queue.isEmpty()) {
+            List<Integer> level = new ArrayList<>();
+            int queueSize = queue.size();
+            
+            for(int i = 0; i < queueSize; i++) {
+                TreeNode node = queue.poll();
+                level.add(node.val);
+                if (node.left != null) queue.offer(node.left);
+                if (node.right != null) queue.offer(node.right);
+            }
+            
+            result.add(level);
+        }
+        
+        return result;
+    }
+}
