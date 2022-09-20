d347598f8cc79615037f533546b42895b97434e5
Chia-Hao (Zack) Cheng <88179158+zchcheng@users.noreply.github.com>
2022-09-20 01:43:01 -0700

    Time: 54 ms, Memory: 43.8 MB - LeetHub

---
+++ implement-strstr/implement-strstr.java
@@ -0,0 +1,51 @@
+class Solution {
+    public int strStr(String haystack, String needle) {
+        int[] lps = buildLps(needle);
+        
+        for(int i = 0, j = 0; i < haystack.length(); i++) {
+            char c = haystack.charAt(i);
+            
+            j = findPattern(lps, c, needle, j);
+            
+            System.out.println(j);
+            
+            if (j == needle.length()) return i - needle.length() + 1;
+        }
+        
+        return -1;
+    }
+    
+    int findPattern(int[] lps, char c, String needle, int idx) {
+        System.out.println(c);
+        
+        while(idx >= 0) {
+            System.out.println(needle.charAt(lps[idx]));
+            if (c == needle.charAt(idx)) return idx + 1;
+            if (idx == 0) break;
+            idx = lps[idx - 1];
+        }
+        
+        return 0;
+    }
+    
+    int[] buildLps(String needle) {
+        int[] lps = new int[needle.length()];
+        
+        for(int i = 1; i < needle.length(); i++) {
+            char c = needle.charAt(i);
+            
+            int idx = i - 1;
+            
+            while(idx >= 0) {
+                if (needle.charAt(lps[idx]) == c) {
+                    lps[i] = lps[idx] + 1;
+                    break;
+                } else {
+                    idx = lps[idx] - 1;
+                }
+            }
+        }
+        
+        return lps;
+    }
+}
