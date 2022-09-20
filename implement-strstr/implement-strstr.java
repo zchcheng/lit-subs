f75057af32096e492e438daf8379a89ecb563941
Chia-Hao (Zack) Cheng <88179158+zchcheng@users.noreply.github.com>
2022-09-20 02:15:13 -0700

    Time: 9 ms, Memory: 42.4 MB - LeetHub

--- implement-strstr/implement-strstr.java
+++ implement-strstr/implement-strstr.java
@@ -2,6 +2,10 @@ class Solution {
     public int strStr(String haystack, String needle) {
         int[] lps = buildLps(needle);
         
+        for(int i : lps) {
+            System.out.println(i);
+        }
+        
         for(int i = 0, j = 0; i < haystack.length(); i++) {
             char c = haystack.charAt(i);
             
@@ -17,7 +21,7 @@ class Solution {
         while(idx >= 0) {
             if (c == needle.charAt(idx)) return idx + 1;
             if (idx == 0) break;
-            idx = lps[idx - 1];
+            idx = lps[idx - 1] + 1;
         }
         
         return 0;
@@ -26,19 +30,20 @@ class Solution {
     int[] buildLps(String needle) {
         int[] lps = new int[needle.length()];
         
-        for(int i = 1; i < needle.length(); i++) {
+        for(int i = 0; i < needle.length(); i++) {
             char c = needle.charAt(i);
             
             int idx = i - 1;
             
             while(idx >= 0) {
-                if (needle.charAt(lps[idx]) == c) {
-                    lps[i] = lps[idx] + 1;
+                if (c == needle.charAt(lps[idx] + 1)) {
+                    idx = lps[idx] + 1;
                     break;
-                } else {
-                    idx = lps[idx] - 1;
                 }
+                idx = lps[idx];
             }
+            
+            lps[i] = idx;
         }
         
         return lps;
