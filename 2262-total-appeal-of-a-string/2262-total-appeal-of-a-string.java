475656a1815a2b9eaaf828584a381436bb837383
Chia-Hao (Zack) Cheng <88179158+zchcheng@users.noreply.github.com>
2022-09-25 23:56:01 -0700

    Time: 30 ms (27.70%), Space: 52.2 MB (22.21%) - LeetHub

--- 2262-total-appeal-of-a-string/2262-total-appeal-of-a-string.java
+++ 2262-total-appeal-of-a-string/2262-total-appeal-of-a-string.java
@@ -4,33 +4,22 @@ class Solution {
         
         Arrays.fill(prevIdx, -1);
         
-        int[][] roc = new int[s.length()][2];
+        int[] roc = new int[s.length()];
         
         for(int i = 0; i < s.length(); i++) {
             int c = s.charAt(i) - 'a';
             int pi = prevIdx[c];
             
-            //if (pi >= 0) roc[pi][1] = i;
-            roc[i] = new int[] { pi + 1, s.length() - 1 };
+            roc[i] = pi + 1;
             prevIdx[c] = i;
         }
-        
-        //print(roc);
         
         long result = 0;
         
         for(int i = 0; i < s.length(); i++) {
-            int[] p = roc[i];
-            long k = (long)(i - (long)p[0] + 1) * (long)((long)p[1] - i + 1);
-            //System.out.println(k);
-            result += k;
+            result += (long)(i - roc[i] + 1) * (s.length() - i);
         }
         
         return result;
-    }
-    
-    void print(int[][] arr) {
-        for(int i = 0; i < arr.length; i++)
-            System.out.println("[" + arr[i][0] + ", " + arr[i][1] + "]");
     }
 }
