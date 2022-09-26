300222a7ac812ab92bb56740442a7a7a97b848e7
Chia-Hao (Zack) Cheng <88179158+zchcheng@users.noreply.github.com>
2022-09-25 22:57:12 -0700

    Time: 1797 ms (5.03%), Space: 148.8 MB (19.09%) - LeetHub

---
+++ 2055-plates-between-candles/2055-plates-between-candles.java
@@ -0,0 +1,83 @@
+class Solution {
+    public int[] platesBetweenCandles(String s, int[][] queries) {
+        List<int[]> list = new ArrayList<>();
+        
+        int[] current = null;
+        for(int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (c == '|') {
+                if (current != null) {
+                    current[1] = i;
+                    list.add(current);
+                }
+                current = new int[] {i, i};
+            }
+        }
+        
+        printList(list);
+        
+        int[] result = new int[queries.length];
+        
+        for(int i = 0; i < queries.length; i++) {
+            result[i] = binSearch(list, queries[i]);
+        }
+        
+        return result;
+    }
+    
+    int binSearch(List<int[]> list, int[] query) {
+        int head = -1;
+        int tail = -1;
+        
+        // upper bound
+        {
+            int l = 0;
+            int r = list.size() - 1;
+            int q = query[0];
+
+            while(l <= r) {
+                int m = l + (r - l) / 2;
+                
+                int[] p = list.get(m);
+                
+                if (p[0] < q) l = m + 1;
+                else r = m - 1;
+            }
+            
+            head = l;
+        }
+        
+        // lower bound
+        {
+            int l = 0;
+            int r = list.size() - 1;
+            int q = query[1];
+
+            while(l <= r) {
+                int m = l + (r - l) / 2;
+                
+                int[] p = list.get(m);
+                
+                if (p[1] <= q) l = m + 1;
+                else r = m - 1;
+            }
+            
+            tail = r;
+        }
+        
+        System.out.println("tail: " + tail);
+        
+        if (head >= list.size() || tail < 0) return 0;
+        
+        int[] ph = list.get(head);
+        int[] pt = list.get(tail);
+        
+        return Math.max(pt[1] - ph[0] - (tail - head + 1), 0);
+    }
+    
+    void printList(List<int[]> list) {
+        for(int i = 0; i < list.size(); i++) {
+            System.out.println(list.get(i)[0] + ", " + list.get(i)[1]);
+        }
+    }
+}
