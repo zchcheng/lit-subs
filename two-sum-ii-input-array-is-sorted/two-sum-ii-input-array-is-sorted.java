215b1216fb1232626a3ba837c013d486efe97b98
Chia-Hao (Zack) Cheng <88179158+zchcheng@users.noreply.github.com>
2022-09-26 16:18:50 -0700

    Time: 4 ms, Memory: 50.5 MB - LeetHub

---
+++ two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int[] twoSum(int[] numbers, int target) {
+        int l = 0, r = numbers.length - 1;
+        
+        while(l < r) {
+            int sum = numbers[l] + numbers[r];
+            if (sum == target) return new int[] {l + 1, r + 1};
+            if (sum > target) r--;
+            else l++;
+        }
+
+        return null;
+    }
+}
