b9271becf36e93796bb99bf4a1d6fc0c28fb049b
Chia-Hao (Zack) Cheng <88179158+zchcheng@users.noreply.github.com>
2022-09-25 22:06:05 -0700

    Time: 49 ms (60.43%), Space: 117.2 MB (54.68%) - LeetHub

--- 973-k-closest-points-to-origin/973-k-closest-points-to-origin.java
+++ 973-k-closest-points-to-origin/973-k-closest-points-to-origin.java
@@ -1,10 +1,9 @@
 class Solution {
     public int[][] kClosest(int[][] points, int k) {
-        PriorityQueue<int[]> pq = new PriorityQueue<int[]>((a, b) -> {
-            return (b[0] * b[0] + b[1] * b[1]) - (a[0] * a[0] + a[1] * a[1]);
-        });
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> - (a[0] * a[0] + a[1] * a[1]) + (b[0] * b[0] + b[1] * b[1]));
         
-        for(int[] p : points) {
+        for(int i = 0; i < points.length; i++) {
+            int[] p = points[i];
             pq.offer(p);
             
             while(pq.size() > k) {
@@ -12,6 +11,12 @@ class Solution {
             }
         }
         
-        return pq.toArray(int[][]::new);
+        int[][] result = new int[k][2];
+        
+        for(int i = 0; i < k; i++) {
+            result[i] = pq.poll();
+        }
+        
+        return result;
     }
 }
