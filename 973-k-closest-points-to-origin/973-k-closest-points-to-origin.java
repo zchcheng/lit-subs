f7de3e43840b157396c34efece66de8a2aa101d5
Chia-Hao (Zack) Cheng <88179158+zchcheng@users.noreply.github.com>
2022-09-25 22:16:08 -0700

    Time: 102 ms (18.62%), Space: 114.1 MB (77.48%) - LeetHub

--- 973-k-closest-points-to-origin/973-k-closest-points-to-origin.java
+++ 973-k-closest-points-to-origin/973-k-closest-points-to-origin.java
@@ -1,22 +1,44 @@
 class Solution {
     public int[][] kClosest(int[][] points, int k) {
-        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> - (a[0] * a[0] + a[1] * a[1]) + (b[0] * b[0] + b[1] * b[1]));
-        
-        for(int i = 0; i < points.length; i++) {
-            int[] p = points[i];
-            pq.offer(p);
-            
-            while(pq.size() > k) {
-                pq.poll();
-            }
-        }
+        quickSelect(points, 0, points.length - 1, k - 1);
         
         int[][] result = new int[k][2];
         
-        for(int i = 0; i < k; i++) {
-            result[i] = pq.poll();
-        }
+        for(int i = 0; i < k; i++) result[i] = points[i];
         
         return result;
+    }
+    
+    void quickSelect(int[][] arr, int l, int r, int k) {
+        int idx = partition(arr, l, r);
+        
+        if (idx == k) return;
+        
+        if (idx > k) quickSelect(arr, l, idx - 1, k);
+        else quickSelect(arr, idx + 1, r, k);
+    }
+    
+    int partition(int[][] arr, int l, int r) {
+        int pivot = cal(arr[l]);
+        
+        int j = l + 1;
+        for(int i = l + 1; i <= r; i++) {
+            int v = cal(arr[i]);
+            if (v <= pivot) swap(arr, i, j++);
+        }
+        
+        swap(arr, l, j - 1);
+        
+        return j - 1;
+    }
+    
+    void swap(int[][] arr, int a, int b) {
+        int[] tmp = arr[a];
+        arr[a] = arr[b];
+        arr[b] = tmp;
+    }
+    
+    int cal(int[] p) {
+        return p[0] * p[0] + p[1] * p[1];
     }
 }
