457c291d26d1bf7979fa30ed085291e00866e9e0
Chia-Hao (Zack) Cheng <88179158+zchcheng@users.noreply.github.com>
2022-09-26 17:17:34 -0700

    Time: 38 ms, Memory: 42.9 MB - LeetHub

--- implement-strstr/implement-strstr.java
+++ implement-strstr/implement-strstr.java
@@ -1,49 +1,53 @@
 class Solution {
     public int strStr(String haystack, String needle) {
-        int[] lps = buildLps(needle);
+        int[] lps = buildLPS(needle);
         
-        for(int i : lps) {
-            System.out.println(i);
-        }
+        for(int n: lps) System.out.println("->" + n);
         
-        for(int i = 0, j = 0; i < haystack.length(); i++) {
-            char c = haystack.charAt(i);
-            
-            j = findPattern(lps, c, needle, j);
-            
-            if (j == needle.length()) return i - needle.length() + 1;
+        for(int i = 0, j = -1; i < haystack.length(); i++) {
+            j = findPattern(lps, haystack.charAt(i), j, needle);
+            if (j == needle.length() - 1) return i - needle.length() + 1;
         }
         
         return -1;
     }
     
-    int findPattern(int[] lps, char c, String needle, int idx) {
-        while(idx >= 0) {
-            if (c == needle.charAt(idx)) return idx + 1;
-            if (idx == 0) break;
+    int findPattern(int[] lps, char c, int idx, String str) {
+        idx++;
+        
+        while(idx >= 0 && str.charAt(idx) != c) {
+            if (idx <= 0) {
+                idx = -1;
+                break;
+            }
             idx = lps[idx - 1] + 1;
+            System.out.println("roll to prev: " + idx);
         }
         
-        return 0;
+        System.out.println(idx);
+        
+        return idx;
     }
     
-    int[] buildLps(String needle) {
-        int[] lps = new int[needle.length()];
+    int[] buildLPS(String str) {
+        int[] lps = new int[str.length()];
         
-        for(int i = 0; i < needle.length(); i++) {
-            char c = needle.charAt(i);
+        Arrays.fill(lps, -1);
+        
+        for(int i = 1; i < str.length(); i++) {
+            char c = str.charAt(i);
             
-            int idx = i - 1;
+            int j = lps[i - 1] + 1;
             
-            while(idx >= 0) {
-                if (c == needle.charAt(lps[idx] + 1)) {
-                    idx = lps[idx] + 1;
+            while(j >= 0 && str.charAt(j) != c) {
+                if (j == 0) {
+                    j = -1;
                     break;
                 }
-                idx = lps[idx];
+                j = lps[j - 1] + 1;
             }
             
-            lps[i] = idx;
+            lps[i] = j;
         }
         
         return lps;
