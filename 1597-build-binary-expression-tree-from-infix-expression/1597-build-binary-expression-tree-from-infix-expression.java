6017cda4550cf301a962d9ffc7969820ae93a467
Chia-Hao (Zack) Cheng <88179158+zchcheng@users.noreply.github.com>
2022-09-29 20:50:43 -0700

    Time: 19 ms (5.17%), Space: 42.6 MB (15.35%) - LeetHub

---
+++ 1597-build-binary-expression-tree-from-infix-expression/1597-build-binary-expression-tree-from-infix-expression.java
@@ -0,0 +1,60 @@
+/**
+ * Definition for a binary tree node.
+ * class Node {
+ *     char val;
+ *     Node left;
+ *     Node right;
+ *     Node() {this.val = ' ';}
+ *     Node(char val) { this.val = val; }
+ *     Node(char val, Node left, Node right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    Stack<Node> numStack = new Stack<Node>();
+    Stack<Node> opStack = new Stack<Node>();
+        
+    public Node expTree(String s) {
+        for(int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            
+            if (Character.isDigit(c)) {
+                numStack.push(new Node(c));
+                continue;
+            }
+            
+            
+            if (c == '+' || c == '-') {
+                while(!opStack.isEmpty() && numStack.size() >= 2 && opStack.peek().val != '(') popOpNode();
+            }
+            
+            if (c == '*' || c == '/') {
+                while(!opStack.isEmpty() && (opStack.peek().val == '*' || opStack.peek().val == '/') && numStack.size() >= 2) popOpNode();
+            }
+            
+            if (c == ')') {
+                while(!opStack.isEmpty() && opStack.peek().val != '(') popOpNode();
+                if (!opStack.isEmpty()) opStack.pop();
+            }
+            
+            if (c != ')') opStack.push(new Node(c));
+        }
+        
+        while(!opStack.isEmpty()) popOpNode();
+        
+        return numStack.peek();
+    }
+    
+    void popOpNode() {
+        Node op = opStack.pop();
+        System.out.println(op.val);
+        op.right = numStack.pop();
+        System.out.println("r: " + op.right.val);
+        op.left = numStack.pop();
+        System.out.println("l: " + op.left.val);
+        numStack.push(op);
+    }
+}
