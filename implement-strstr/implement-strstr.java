fad55f55d5038c54879983ff8eabf4155eaab393
Chia-Hao (Zack) Cheng <88179158+zchcheng@users.noreply.github.com>
2022-10-01 16:21:20 -0700

    Time: 1 ms, Memory: 42.3 MB - LeetHub

--- implement-strstr/implement-strstr.java
+++ implement-strstr/implement-strstr.java
@@ -1,6 +1,6 @@
 class Solution {
     public int strStr(String haystack, String needle) {
-        int[] lps = buildLPS(needle);
+        int[] lps = buildLps(needle);
         
         for(int i = 0, j = -1; i < haystack.length(); i++) {
             j = findPattern(lps, haystack.charAt(i), j, needle);
@@ -10,21 +10,7 @@ class Solution {
         return -1;
     }
     
-    int findPattern(int[] lps, char c, int idx, String str) {
-        idx++;
-        
-        while(idx >= 0 && str.charAt(idx) != c) {
-            if (idx <= 0) {
-                idx = -1;
-                break;
-            }
-            idx = lps[idx - 1] + 1;
-        }
-        
-        return idx;
-    }
-    
-    int[] buildLPS(String str) {
+    int[] buildLps(String str) {
         int[] lps = new int[str.length()];
         
         Arrays.fill(lps, -1);
@@ -33,18 +19,20 @@ class Solution {
             char c = str.charAt(i);
             
             int j = lps[i - 1] + 1;
-            
-            while(j >= 0 && str.charAt(j) != c) {
-                if (j == 0) {
-                    j = -1;
-                    break;
-                }
-                j = lps[j - 1] + 1;
-            }
+            while(j >= 0 && str.charAt(j) != c) j = (j == 0)? -1 : lps[j - 1] + 1;
             
             lps[i] = j;
         }
         
         return lps;
+    }
+    
+    int findPattern(int[] lps, char c, int idx, String pat) {
+        idx++;
+        while(idx >= 0 && pat.charAt(idx) != c) {
+            if (idx == 0) idx = -1;
+            else idx = lps[idx - 1] + 1;
+        }
+        return idx;
     }
 }
