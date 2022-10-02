f39d81f7bb90e9f29c85770f260bbe3a46d6e1f7
Chia-Hao (Zack) Cheng <88179158+zchcheng@users.noreply.github.com>
2022-10-01 18:28:59 -0700

    Time: 23 ms, Memory: 44 MB - LeetHub

--- most-common-word/most-common-word.java
+++ most-common-word/most-common-word.java
@@ -1,40 +1,26 @@
 class Solution {
     public String mostCommonWord(String paragraph, String[] banned) {
+        for(char c : new char[] { '!', '?', '\'', ',', ';', '.'}) paragraph = paragraph.replace(c + "", " ");
+        
+        System.out.println(paragraph);
+        
+        String[] words = paragraph.split(" ");
+        
+        Set<String> bannedWords = new HashSet<>();
+        for(String b : banned) bannedWords.add(b);
+        
         Map<String, Integer> freq = new HashMap<>();
-        Set<String> bs = new HashSet<>();
-        
-        Set<Character> symbols = Set.of('!', '?', ',', '\'', ';', '.', ' ');
-        
-        for(String b : banned) bs.add(b);
-        
-        String current = "";
-        for(int i = 0; i < paragraph.length(); i++) {
-            char c = paragraph.charAt(i);
-            if (symbols.contains(c)) {
-                if (!current.isEmpty() && !bs.contains(current)) 
-                    freq.put(current, freq.getOrDefault(current, 0) + 1);
-                current = "";
-                continue;
-            }
-            
-            if (c < 'a') {
-                c = (char)('a' + (int)(c - 'A'));
-            }
-            
-            current += c;
-        }
-        
-        if (!current.isEmpty() && !bs.contains(current)) 
-            freq.put(current, freq.getOrDefault(current, 0) + 1);
-        
         String result = "";
-        int f = 0;
         
-        for(Map.Entry<String, Integer> kv : freq.entrySet()) {
-            if (kv.getValue() <= f) continue;
+        for(String w : words) {
+            if (w.isEmpty()) continue;
             
-            result = kv.getKey();
-            f = kv.getValue();
+            w = w.toLowerCase();
+            
+            if (bannedWords.contains(w)) continue;
+            
+            freq.put(w, freq.getOrDefault(w, 0) + 1);
+            if (result.isEmpty() || freq.get(result) < freq.get(w)) result = w;
         }
         
         return result;
