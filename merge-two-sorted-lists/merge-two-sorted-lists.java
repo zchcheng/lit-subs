ea3f42e8f6e2c682eaba7cd07e1f37bbb304abff
Chia-Hao (Zack) Cheng <88179158+zchcheng@users.noreply.github.com>
2022-10-02 11:37:14 -0700

    Time: 0 ms, Memory: 43.5 MB - LeetHub

--- merge-two-sorted-lists/merge-two-sorted-lists.java
+++ merge-two-sorted-lists/merge-two-sorted-lists.java
@@ -10,11 +10,11 @@
  */
 class Solution {
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
-        ListNode result = new ListNode();
-        ListNode current = result;
+        ListNode head = new ListNode();
+        ListNode current = head;
         
         while(list1 != null || list2 != null) {
-            if (list1 == null || (list2 != null) && list1.val >= list2.val) {
+            if (list1 == null || (list2 != null && list1.val > list2.val)) {
                 current.next = list2;
                 list2 = list2.next;
             } else {
@@ -25,8 +25,6 @@ class Solution {
             current = current.next;
         }
         
-        current.next = null;
-        
-        return result.next;
+        return head.next;
     }
 }
