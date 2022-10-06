97a25ed751915580394c11ae5a4da35a7051fb97
Chia-Hao (Zack) Cheng <88179158+zchcheng@users.noreply.github.com>
2022-10-05 20:04:43 -0700

    Time: 0 ms (100.00%), Space: 54.4 MB (36.74%) - LeetHub

--- 704-binary-search/704-binary-search.java
+++ 704-binary-search/704-binary-search.java
@@ -4,9 +4,7 @@ class Solution {
         
         while(l <= r) {
             int m = l + (r - l) / 2;
-            
             if (nums[m] == target) return m;
-            
             if (nums[m] < target) l = m + 1;
             else r = m - 1;
         }
