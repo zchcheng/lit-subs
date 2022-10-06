080721db5e8d4768988b3c98cb97234d9fed4390
Chia-Hao (Zack) Cheng <88179158+zchcheng@users.noreply.github.com>
2022-10-06 14:17:50 -0700

    Time: 1 ms (79.72%), Space: 41.9 MB (90.67%) - LeetHub

--- 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.java
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.java
@@ -6,14 +6,12 @@ class Solution {
             int m = l + (r - l) / 2;
             
             if (nums[m] == target) return m;
-            if (nums[l] == target) return l;
-            if (nums[r] == target) return r;
             
-            if (nums[l] < nums[m]) {
-                if (nums[l] < target && target < nums[m]) r = m - 1;
+            if (nums[l] <= nums[m]) {
+                if (nums[l] <= target && target <= nums[m]) r = m - 1;
                 else l = m + 1;
             } else {
-                if (nums[m] < target && target < nums[r]) l = m + 1;
+                if (nums[m] <= target && target <= nums[r]) l = m + 1;
                 else r = m - 1;
             }
         }
