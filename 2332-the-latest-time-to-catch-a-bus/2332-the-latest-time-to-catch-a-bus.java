6af90d158486442265eeab86f6d97801e6eb4fee
Chia-Hao (Zack) Cheng <88179158+zchcheng@users.noreply.github.com>
2022-10-14 13:13:26 -0700

    Time: 88 ms (26.90%), Space: 95.6 MB (29.95%) - LeetHub

---
+++ 2332-the-latest-time-to-catch-a-bus/2332-the-latest-time-to-catch-a-bus.java
@@ -0,0 +1,35 @@
+class Solution {
+    public int latestTimeCatchTheBus(int[] buses, int[] passengers, int capacity) {
+        Arrays.sort(buses);
+        Arrays.sort(passengers);
+        
+        int pi = 0;
+        int result = 0;
+        
+        int pb = 0;
+        for(int b : buses) {
+            int cnt = 0;
+            
+            Stack<Integer> stack = new Stack();
+            
+            for(; cnt < capacity && pi < passengers.length && passengers[pi] <= b; cnt++, pi++) {
+                stack.push(passengers[pi]);
+            }
+            
+            //System.out.println(stack + " " + pb);
+            
+            int tmp = (stack.size() == capacity)? stack.peek() : b;
+            
+            int npb = pb;
+            while(!stack.isEmpty() && stack.peek() == tmp) {
+                npb = Math.max(npb, stack.peek());
+                tmp = stack.pop() - 1;
+            }
+            
+            if (tmp > pb) result = tmp;
+            pb = npb;
+        }
+        
+        return result;
+    }
+}
