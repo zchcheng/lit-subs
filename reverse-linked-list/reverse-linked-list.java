e18b6a30121dd71e93673df49de5bf319e9daa8d
Chia-Hao (Zack) Cheng <88179158+zchcheng@users.noreply.github.com>
2022-10-15 14:43:16 -0700

    Time: 1 ms, Memory: 42.9 MB - LeetHub

--- reverse-linked-list/reverse-linked-list.java
+++ reverse-linked-list/reverse-linked-list.java
@@ -10,10 +10,6 @@
  */
 class Solution {
     public ListNode reverseList(ListNode head) {
-        return reverseList_recursionVersion(null, head);
-    }
-    
-    ListNode reverseList_iterateVersion(ListNode head) {
         ListNode result = new ListNode();
         
         while(head != null) {
@@ -26,17 +22,5 @@ class Solution {
         }
         
         return result.next;
-    }
-    
-    ListNode reverseList_recursionVersion(ListNode prev, ListNode head) {
-        if (head == null) return null;
-        if (head.next == null) return head;
-        
-        ListNode newHead = reverseList_recursionVersion(head, head.next);
-        
-        head.next.next = head;
-        head.next = prev;
-        
-        return newHead;
     }
 }
