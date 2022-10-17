89ab9902e4bf4715b3b47f04639b972217fc63ba
Chia-Hao (Zack) Cheng <88179158+zchcheng@users.noreply.github.com>
2022-10-17 14:51:03 -0700

    Time: 0 ms, Memory: 40.5 MB - LeetHub

--- symmetric-tree/symmetric-tree.java
+++ symmetric-tree/symmetric-tree.java
@@ -15,13 +15,15 @@
  */
 class Solution {
     public boolean isSymmetric(TreeNode root) {
-        if (root == null) return true;
         return helper(root.left, root.right);
     }
     
-    boolean helper(TreeNode root1, TreeNode root2) {
-        if (root1 == null) return root2 == null;
-        if (root2 == null) return false;
-        return root1.val == root2.val && helper(root1.left, root2.right) && helper(root1.right, root2.left);
+    boolean helper(TreeNode left, TreeNode right) {
+        if (left == null) return right == null;
+        if (right == null) return false;
+        
+        if (left.val != right.val) return false;
+        
+        return helper(left.left, right.right) && helper(left.right, right.left);
     }
 }
