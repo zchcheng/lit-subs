94e351645e51c2a19172604712ffdb0157688e77
Chia-Hao (Zack) Cheng <88179158+zchcheng@users.noreply.github.com>
2022-10-17 17:26:00 -0700

    Time: 133 ms (84.95%), Space: 46.8 MB (87.66%) - LeetHub

---
+++ 0204-count-primes/0204-count-primes.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int countPrimes(int n) {
+        boolean[] notPrimes = new boolean[n];
+        int result = 0;
+        
+        for(int i = 2; i < n; i++) {
+            if (notPrimes[i - 1]) continue;
+            result += 1;
+            
+            if (i * i > n) continue;
+            
+            for(int j = 2; j * i < n; j++) {
+                notPrimes[j * i - 1] = true;
+            }
+        }
+        
+        return result;
+    }
+}
