5cbddb9d7e823c0e62c7c5676d96ae894aa48c0a
Chia-Hao (Zack) Cheng <88179158+zchcheng@users.noreply.github.com>
2022-10-18 15:47:19 -0700

    Time: 1 ms (97.43%), Space: 54.5 MB (19.33%) - LeetHub

---
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
@@ -0,0 +1,45 @@
+class Solution {
+    public void setZeroes(int[][] matrix) {
+        int r = matrix.length;
+        int c = matrix[0].length;
+        
+        // elements in the first row should be zero
+        boolean fr = false;
+        
+        // elements in the first col should be zero
+        boolean fc = false;
+        
+        for(int i = 0; i < r; i++) {
+            for(int j = 0; j < c; j++) {
+                if (matrix[i][j] == 0) {
+                    if (i == 0) fr = true;
+                    if (j == 0) fc = true;
+                    matrix[i][0] = 0;
+                    matrix[0][j] = 0;
+                }
+            }
+        }
+        
+        for(int i = 1; i < r; i++) {
+            if (matrix[i][0] != 0) continue;
+            for(int j = 0; j < c; j++) matrix[i][j] = 0;
+        }
+        
+        for(int i = 1; i < c; i++) {
+            if (matrix[0][i] != 0) continue;
+            for(int j = 0; j < r; j++) matrix[j][i] = 0;
+        }
+        
+        if (fr) for(int i = 0; i < c; i++) matrix[0][i] = 0;
+        if (fc) for(int i = 0; i < r; i++) matrix[i][0] = 0;
+    }
+    
+    void println(int[][] m) {
+        for(int i = 0; i < m.length; i++) {
+            for(int j = 0; j < m[0].length; j++) {
+                System.out.print(m[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
