b006ca92be135fa9e4861cf4cd099619f53ec138
Chia-Hao (Zack) Cheng <88179158+zchcheng@users.noreply.github.com>
2022-10-25 10:38:20 -0700

    Time: 56 ms (56.86%), Space: 13.8 MB (97.34%) - LeetHub

--- 0198-house-robber/0198-house-robber.py
+++ 0198-house-robber/0198-house-robber.py
@@ -3,6 +3,6 @@ class Solution:
         dp = [0, 0]
         
         for i in nums:
-            dp[0], dp[1] = dp[1], max(dp[0] + i, dp[1])
+            dp = [dp[1], max(dp[0] + i, dp[1])]
             
         return dp[1]
