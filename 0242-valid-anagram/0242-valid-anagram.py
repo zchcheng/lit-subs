52bd0c4f8727fed6ba9935c06cabb31434f464a2
Chia-Hao (Zack) Cheng <88179158+zchcheng@users.noreply.github.com>
2022-10-24 23:23:39 -0700

    Time: 66 ms (78.24%), Space: 14.3 MB (99.66%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,13 +1,3 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        return self.encode(s) == self.encode(t)
-        
-        
-    def encode(self, s: str) -> str:
-        b = [0] * 26
-        for c in s:
-            b[ord(c) - ord('a')] += 1
-        r = ''
-        for i in b:
-            r += str(b) + ' '
-        return r
+        return collections.Counter(s) == collections.Counter(t)
