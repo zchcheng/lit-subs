51bfd22689cb6fd4826e0da6f11bc0eb53a8e1b7
Chia-Hao (Zack) Cheng <88179158+zchcheng@users.noreply.github.com>
2022-10-25 20:53:39 -0700

    Time: 554 ms, Memory: 15.3 MB - LeetHub

---
+++ missing-number/missing-number.py
@@ -0,0 +1,24 @@
+class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
+        n = len(nums)
+        
+        for i in range(n):
+            k = abs(nums[i])
+            if k == n + 1:
+                k = 0
+                
+            if k >= n:
+                continue
+                
+            if nums[k] == 0:
+                nums[k] = - n - 1
+            else:
+                nums[k] = -abs(nums[k])
+                
+        print(nums)
+                
+        for i, j in enumerate(nums):
+            if j >= 0:
+                return i
+            
+        return n
