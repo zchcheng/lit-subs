1c782635d155339af2a32787346522e6023042c2
Chia-Hao (Zack) Cheng <88179158+zchcheng@users.noreply.github.com>
2022-10-27 14:05:52 -0700

    Time: 60 ms (30.04%), Space: 13.8 MB (97.99%) - LeetHub

--- 0394-decode-string/0394-decode-string.py
+++ 0394-decode-string/0394-decode-string.py
@@ -1,32 +1,31 @@
 class Solution:
     def decodeString(self, s: str) -> str:
-        res = ''
-        
-        idx = 0
+        stack = []
+        cur_string = ''
+        index = 0
         times = 0
         
-        while idx < len(s):
-            c = s[idx]
-            if c.isdigit():
-                times = times * 10 + int(c)
-            elif c == '[':
-                j = idx + 1
-                st = 1
-                while st > 0:
-                    if s[j] == ']': st -= 1
-                    elif s[j] == '[': st += 1
-                    if st != 0: j += 1
-                subs = self.decodeString(s[idx + 1 : j])
-                for _ in range(times):
-                    res += subs
-                times = 0    
-                idx = j
-            else:
-                res += c
-            idx += 1
-        return res
+        while index < len(s):
+            c = s[index]
             
-                
+            if c == '[':
+                stack.append((cur_string, times))
+                times = 0
+                cur_string = ''
+            elif c == ']':
+                prev_string, prev_times = stack.pop()
+                for _ in range(prev_times):
+                    prev_string += cur_string
+                cur_string = prev_string
+                times = 0
+            elif c.isdigit():
+                times = times * 10 + int(c)
+            else:
+                cur_string += c
+            index += 1
+            
+        return cur_string
+        
                 
         
         
