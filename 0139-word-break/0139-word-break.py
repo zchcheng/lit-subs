04e4212c6bd93dcd169e2851149923996e06d2fc
Chia-Hao (Zack) Cheng <88179158+zchcheng@users.noreply.github.com>
2022-10-26 21:31:49 -0700

    Time: 84 ms (32.91%), Space: 14.2 MB (14.08%) - LeetHub

---
+++ 0139-word-break/0139-word-break.py
@@ -0,0 +1,19 @@
+class Solution:
+    st = ''
+    wd = []
+    
+    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+        self.st = s
+        self.wd = wordDict
+        return self.helper(0)
+        
+        
+    @cache    
+    def helper(self, i) -> bool:
+        if i >= len(self.st):
+            return True
+        for w in self.wd:
+            if self.st[i:i+len(w)] == w and self.helper(i + len(w)):
+                return True
+        return False
+        
