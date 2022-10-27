89f56a673827fd69076f650d5b2351665442be15
Chia-Hao (Zack) Cheng <88179158+zchcheng@users.noreply.github.com>
2022-10-26 21:16:38 -0700

    Time: 76 ms (73.71%), Space: 15.6 MB (6.21%) - LeetHub

--- 0010-regular-expression-matching/0010-regular-expression-matching.py
+++ 0010-regular-expression-matching/0010-regular-expression-matching.py
@@ -1,12 +1,23 @@
 class Solution:
-    @cache
+    text = ''
+    pat = ''
+    
     def isMatch(self, s: str, p: str) -> bool:
-        if not p:
-            return not s
+        self.pat = p
+        self.text = s
+        return self.helper(0, 0)
         
-        match = len(s) > 0 and p[0] in {s[0], '.'}
+    @cache
+    def helper(self, i, j) -> bool:
         
-        if len(p) >= 2 and p[1] == '*':
-            return self.isMatch(s, p[2:]) or (match and self.isMatch(s[1:], p))
+        if j >= len(self.pat):
+            return i >= len(self.text)
+        
+        match = i < len(self.text) and self.pat[j] in { self.text[i], '.' }
+        
+        if j + 1 < len(self.pat) and self.pat[j + 1] == '*':
+            return self.helper(i, j + 2) or (match and self.helper(i + 1, j))
         else:
-            return match and self.isMatch(s[1:], p[1:])
+            return match and self.helper(i + 1, j + 1)
+            
+        
