6a73492087c834532da813bc360937534b82f06a
Chia-Hao (Zack) Cheng <88179158+zchcheng@users.noreply.github.com>
2022-10-29 11:23:10 -0700

    Time: 1199 ms (7.13%), Space: 67.2 MB (5.78%) - LeetHub

--- 0583-delete-operation-for-two-strings/0583-delete-operation-for-two-strings.py
+++ 0583-delete-operation-for-two-strings/0583-delete-operation-for-two-strings.py
@@ -1,19 +1,25 @@
 class Solution:
+    s1 = None
+    s2 = None
+    
     def minDistance(self, word1: str, word2: str) -> int:
-        return self.helper(word1, word2)
+        lw1 = len(word1)
+        lw2 = len(word2)
+        self.s1 = word1
+        self.s2 = word2
+        return self.helper(0, 0)
         
-
     @cache
-    def helper(self, s1, s2):
-        if not s1 and not s2:
+    def helper(self, i1, i2):
+        if i1 >= len(self.s1) and i2 >= len(self.s2):
             return 0
         
-        if not s1:
-            return len(s2)
-        if not s2:
-            return len(s1)
+        if i1 >= len(self.s1):
+            return len(self.s2) - i2
         
-        if s1[0] == s2[0]:
-            return self.helper(s1[1:], s2[1:])
+        if i2 >= len(self.s2):
+            return len(self.s1) - i1
         
-        return 1 + min(self.helper(s1[1:], s2), self.helper(s1, s2[1:]))
+        if self.s1[i1] == self.s2[i2]:
+            return self.helper(i1 + 1, i2 + 1)
+        return min(self.helper(i1 + 1, i2), self.helper(i1, i2 + 1)) + 1
