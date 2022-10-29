633c38e5f9538731509cc9e9798c409ec62fafa7
Chia-Hao (Zack) Cheng <88179158+zchcheng@users.noreply.github.com>
2022-10-29 11:30:20 -0700

    Time: 1160 ms (7.83%), Space: 17.1 MB (50.97%) - LeetHub

--- 0583-delete-operation-for-two-strings/0583-delete-operation-for-two-strings.py
+++ 0583-delete-operation-for-two-strings/0583-delete-operation-for-two-strings.py
@@ -1,4 +1,5 @@
 class Solution:
+    memo = None
     s1 = None
     s2 = None
     
@@ -7,19 +8,21 @@ class Solution:
         lw2 = len(word2)
         self.s1 = word1
         self.s2 = word2
+        self.memo = [ [-1 for _ in range(lw2 + 1) ] for _ in range(lw1 + 1) ]
         return self.helper(0, 0)
         
-    @cache
     def helper(self, i1, i2):
+        if self.memo[i1][i2] != -1:
+            return self.memo[i1][i2]
+        
         if i1 >= len(self.s1) and i2 >= len(self.s2):
-            return 0
-        
-        if i1 >= len(self.s1):
-            return len(self.s2) - i2
-        
-        if i2 >= len(self.s2):
-            return len(self.s1) - i1
-        
-        if self.s1[i1] == self.s2[i2]:
-            return self.helper(i1 + 1, i2 + 1)
-        return min(self.helper(i1 + 1, i2), self.helper(i1, i2 + 1)) + 1
+            self.memo[i1][i2] = 0
+        elif i1 >= len(self.s1):
+            self.memo[i1][i2] = len(self.s2) - i2
+        elif i2 >= len(self.s2):
+            self.memo[i1][i2] = len(self.s1) - i1
+        elif self.s1[i1] == self.s2[i2]:
+            self.memo[i1][i2] = self.helper(i1 + 1, i2 + 1)
+        else: self.memo[i1][i2] = min(self.helper(i1 + 1, i2), self.helper(i1, i2 + 1)) + 1
+            
+        return self.memo[i1][i2]
