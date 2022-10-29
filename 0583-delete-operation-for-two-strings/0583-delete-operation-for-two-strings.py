3388e15ddea5f135795e1c841f3c0e62de0ab376
Chia-Hao (Zack) Cheng <88179158+zchcheng@users.noreply.github.com>
2022-10-29 11:49:15 -0700

    Time: 401 ms (77.37%), Space: 16 MB (67.24%) - LeetHub

--- 0583-delete-operation-for-two-strings/0583-delete-operation-for-two-strings.py
+++ 0583-delete-operation-for-two-strings/0583-delete-operation-for-two-strings.py
@@ -1,28 +1,23 @@
 class Solution:
-    memo = None
-    s1 = None
-    s2 = None
-    
     def minDistance(self, word1: str, word2: str) -> int:
-        lw1 = len(word1)
-        lw2 = len(word2)
-        self.s1 = word1
-        self.s2 = word2
-        self.memo = [ [-1 for _ in range(lw2 + 1) ] for _ in range(lw1 + 1) ]
-        return self.helper(0, 0)
+        # [len(word1) + 1][len(word2) + 1]
+        dp = [ [0 for _ in range(len(word2) + 1)] for _ in range(len(word1) + 1) ]
         
-    def helper(self, i1, i2):
-        if self.memo[i1][i2] != -1:
-            return self.memo[i1][i2]
+        #    '' a b c
+        # ''  0 0 0 0
+        #  a  0 1 1 1
+        #  c  0 1 1 2
         
-        if i1 >= len(self.s1) and i2 >= len(self.s2):
-            self.memo[i1][i2] = 0
-        elif i1 >= len(self.s1):
-            self.memo[i1][i2] = len(self.s2) - i2
-        elif i2 >= len(self.s2):
-            self.memo[i1][i2] = len(self.s1) - i1
-        elif self.s1[i1] == self.s2[i2]:
-            self.memo[i1][i2] = self.helper(i1 + 1, i2 + 1)
-        else: self.memo[i1][i2] = min(self.helper(i1 + 1, i2), self.helper(i1, i2 + 1)) + 1
+        for i in range(len(word1)):
+            c1 = word1[i]
             
-        return self.memo[i1][i2]
+            for j in range(len(word2)):
+                c2 = word2[j]
+                
+                if c1 == c2:
+                    dp[i + 1][j + 1] = dp[i][j] + 1
+                else:
+                    dp[i + 1][j + 1] = max(dp[i][j + 1], dp[i + 1][j])
+                    
+        return len(word1) + len(word2) - 2 * dp[len(word1)][len(word2)]
+        
