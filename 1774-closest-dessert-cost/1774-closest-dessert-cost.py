6383688a124d56fab410e1d0b3b317ad6f3ce34a
Chia-Hao (Zack) Cheng <88179158+zchcheng@users.noreply.github.com>
2022-10-31 15:21:40 -0700

    Time: 410 ms (68.86%), Space: 13.9 MB (77.76%) - LeetHub

--- 1774-closest-dessert-cost/1774-closest-dessert-cost.py
+++ 1774-closest-dessert-cost/1774-closest-dessert-cost.py
@@ -1,56 +1,20 @@
 class Solution:
-    tcs = None
-    used = None
-    target = None
+    best = None
     
     def closestCost(self, baseCosts: List[int], toppingCosts: List[int], target: int) -> int:
-        self.tcs = toppingCosts
-        self.used = [0] * len(toppingCosts)
-        
-        res = None
-        
-        for c in baseCosts:
-            self.target = target - c
-            cost = self.backtracking(0, 0)
+        self.best = target - baseCosts[0]
+        for bc in baseCosts:
+            self.backtracking(toppingCosts, 0, target - bc)
+        return target - self.best
             
-            tmp = cost + c
-            
-            if res is None:
-                res = tmp
-                continue
-            
-            d1 = abs(tmp - target)
-            d2 = abs(res - target)
-            
-            if d1 < d2:
-                res = tmp
-            elif d2 < d1:
-                continue
-            else:
-                res = min(tmp, res)
-
-        return res
-            
-            
-    def backtracking(self, i, current):
-        if i >= len(self.tcs):
-            return current
         
-        c1 = current
-        
-        if self.used[i] < 2:
-            self.used[i] += 1
-            c1 = self.backtracking(i, current + self.tcs[i])
-            self.used[i] -= 1
+    def backtracking(self, tcs, idx, cur):
+        if abs(cur) < abs(self.best) or (abs(cur) == abs(self.best) and cur > self.best):
+            self.best = cur
             
-        c2 = self.backtracking(i + 1, current)
+        if idx >= len(tcs) or cur <= 0:
+            return cur
         
-        d1 = abs(c1 - self.target)
-        d2 = abs(c2 - self.target)
-        
-        if d1 == d2:
-            return min(c1, c2)
-        if d1 < d2:
-            return c1
-        return c2
-        
+        self.backtracking(tcs, idx + 1, cur)
+        self.backtracking(tcs, idx + 1, cur - tcs[idx])
+        self.backtracking(tcs, idx + 1, cur - 2 * tcs[idx])
