5547b461dbc0172626301f7f235e7295d6a717c9
Chia-Hao (Zack) Cheng <88179158+zchcheng@users.noreply.github.com>
2023-08-13 14:01:12 -0700

    Time: 120 ms (11.02%), Space: 30.9 MB (5.64%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.cpp
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -1,38 +1,38 @@
 class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        vector<vector<string>> result;
         unordered_map<string, vector<string>> map;
-        
-        for(string s : strs) {
-            string encoded = encode(s);
-            if (map.find(encoded) == map.end()) {
-                map[encoded] = {};
+
+        for (string& str : strs) {
+            string key = encode(str);
+
+            auto it = map.find(key);
+
+            if (it == map.end()) {
+                map[key] = {str};
+            } else {
+                map[key].push_back(str);
             }
-            map[encoded].emplace_back(s);
         }
-        
-        vector<vector<string>> res;
-        
-        for(auto &kv : map) {
-            res.emplace_back(kv.second);
+
+        for (auto it : map) {
+            result.push_back(it.second);
         }
-        
-        return res;
+
+        return result;
     }
-    
-    string encode(string &s) {
-        int bucket[26] = {};
-        
-        for (int i = 0; i < s.size(); i++) {
-            bucket[s[i] - 'a']++;
+
+    string encode(string& str) {
+        string result;
+        vector<int> arr(26, 0);
+        for (char& i : str) {
+            arr[i - 'a']++;
         }
-        
-        string res;
-        
         for (int i = 0; i < 26; i++) {
-            res += to_string(bucket[i]) + ",";
+            result.append(to_string(arr[i]));
+            result.append(",");
         }
-        
-        return res;
+        return result;
     }
 };
