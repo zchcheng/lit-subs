d7ce6c2fc2c9cb7e74cf4f05ad15cd241a2104e0
Chia-Hao (Zack) Cheng <88179158+zchcheng@users.noreply.github.com>
2023-08-13 13:16:51 -0700

    Time: 3 ms (98.29%), Space: 7.3 MB (81.99%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,23 +1,19 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        vector<int> v1 = count(s);
-        vector<int> v2 = count(t);
-        
-        for (int i = 0; i < 26; i++) {
-            if (v1[i] != v2[i]) return false;
+        vector<int> arr (26, 0);
+
+        if (s.size() != t.size()) return false;
+
+        for (int i = 0; i < s.size(); i++) {
+            arr[s[i] - 'a']++;
+            arr[t[i] - 'a']--;
         }
-        
+
+        for (int i : arr) {
+            if (i != 0) return false;
+        }
+
         return true;
-    }
-    
-    vector<int> count(string &s) {
-        vector<int> bucket(26, 0);
-        
-        for(int i = 0; i < s.size(); i++) {
-            bucket[s[i] - 'a']++;
-        }
-        
-        return bucket;
     }
 };
